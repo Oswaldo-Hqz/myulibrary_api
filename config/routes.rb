62083daf657852fe844883c1e00bd3fb98da5aac
Oswaldo Henriqz <oehero@hotmail.com>
2021-10-10 21:22:26 -0600

    user router modified for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resource :users, only: [:create]
+      post "/login", to: "users#login"
+      get "/auto_login", to: "users#auto_login"
     end
   end
   namespace :api do
