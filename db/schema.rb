02bf94e7d4a0a0ad00ccad67c09d22fd9c8f93ca
Oswaldo Henriqz <oehero@hotmail.com>
2021-10-10 19:08:38 -0600

    column name modification

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2021_10_10_201719) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.integer "role"
+    t.integer "role_id"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
